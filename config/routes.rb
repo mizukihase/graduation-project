67b30b62237d713a3d14cef86baebb10c7702701
mizukihase <mrthjmt2015@gmail.com>
2025-05-27 00:18:39 +0900

    マイリスト

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
   resources :books, only: [ :index ]
   resources :mangas, only: [ :index ]
   resources :movies, only: [ :index ]
+  resources :user_productions, only: [:create, :destroy]
   get "recommended_works", to: "works#recommended", as: "recommended_works"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get 'my_list', to: 'user_productions#index', as: :my_list
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
