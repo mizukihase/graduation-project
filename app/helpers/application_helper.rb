efcfa8be62a72382849995a05dd502affb39e0ed
mizukihase <mrthjmt2015@gmail.com>
2025-06-30 00:10:42 +0900

    静的OGP追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,23 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "おすすめ作品"
+      site: 'おすすめ作品紹介アプリ',
+      title: '作品の提案サービス',
+      reverse: true,
+      charset: 'utf-8',
+      canonical: request.original_url,
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        local: 'ja-JP'
+      },
+      twitter: {
+        site: '@iif2Wi5kSY53645',
+      }
     }
   end
 end
